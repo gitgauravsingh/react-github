ced39bd9adc96f38df96d5ad2fb9845945aa6e71
gitgauravsingh <gauravsurvanshi2002@gmail.com>
2024-08-17 02:59:18 +0530

    update

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -69,32 +69,57 @@ const Header = () => {
 
   return (
     <>
-      <StickyBox>
-        <div className={Style.headerbar}>
-          <div className={Style.bannerimg}>
-            <img src={Img} alt="" />
+      <div className={Style.headerbar}>
+        <div className={Style.bannerimg}>
+          <img src={Img} alt="" />
+        </div>
+        {overlay && (
+          <div className={Style.overlaybox} id={Style.overlay}>
+            <Form handelclick={handleoverlay} />
           </div>
-          {overlay && (
-            <div className={Style.overlaybox} id={Style.overlay}>
-              <Form handelclick={handleoverlay} />
-            </div>
-          )}
+        )}
 
-          <div className={Style.mainnav}>
-            <div className={Style.mainhead}>
-              <div
-                className={Style.responsibe}
-                id="slideOpen"
-                onClick={navOpen}
-              >
-                <span></span>
-                <span></span>
-                <span></span>
+        <div className={Style.mainnav}>
+          <div className={Style.mainhead}>
+            <div className={Style.responsibe} id="slideOpen" onClick={navOpen}>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <div className={Style.logo}>
+              <img src={Imglogo} alt="Logo" />
+            </div>
+            <div className={Style.navbar}>
+              <ul>
+                {detailbar.data &&
+                  detailbar.data.map((item) => (
+                    <>
+                      {item.header &&
+                        item.header.map((headerItem) => (
+                          <li key={headerItem.title}>
+                            <a href="#">
+                              {headerItem.title}
+                              <i className="fa-solid fa-angle-down"></i>
+                            </a>
+                          </li>
+                        ))}
+                    </>
+                  ))}
+              </ul>
+            </div>
+            <div
+              className={Style.crossbar}
+              id="closeslider"
+              onClick={closeNav}
+              style={{ display: "none" }}
+            >
+              <span>X</span>
+            </div>
+            <div className={Style.mainheadres} id="slider">
+              <div className={Style.searchbardata}>
+                <input type="search" placeholder="Search" />
               </div>
-              <div className={Style.logo}>
-                <img src={Imglogo} alt="Logo" />
-              </div>
-              <div className={Style.navbar}>
+              <div className={Style.homebar}>
                 <ul>
                   {detailbar.data &&
                     detailbar.data.map((item) => (
@@ -112,88 +137,57 @@ const Header = () => {
                     ))}
                 </ul>
               </div>
-              <div
-                className={Style.crossbar}
-                id="closeslider"
-                onClick={closeNav}
-                style={{ display: "none" }}
-              >
-                <span>X</span>
-              </div>
-              <div className={Style.mainheadres} id="slider">
-                <div className={Style.searchbardata}>
-                  <input type="search" placeholder="Search" />
-                </div>
-                <div className={Style.homebar}>
-                  <ul>
-                    {detailbar.data &&
-                      detailbar.data.map((item) => (
-                        <>
-                          {item.header &&
-                            item.header.map((headerItem) => (
-                              <li key={headerItem.title}>
-                                <a href="#">
-                                  {headerItem.title}
-                                  <i className="fa-solid fa-angle-down"></i>
-                                </a>
-                              </li>
-                            ))}
-                        </>
-                      ))}
-                  </ul>
-                </div>
-              </div>
-              <div className={Style.iconbar}>
-                <i className="fa-brands fa-whatsapp"></i>
-              </div>
-              <div className={Style.inquery} onClick={showDiv}>
-                <a href="#/">INQUIRE NOW</a>
-                <div className={Style.emailbar}>
-                  <i className="fa-solid fa-envelope"></i>
-                </div>
-              </div>
-              <div className={Style.searchbar}>
-                <div className={Style.searchvalue}>
-                  <input
-                    type="search"
-                    placeholder="Search"
-                    value={search}
-                    onChange={handleChange}
-                  />
-                  <div className={Style.mainserch}>
-                    {query && (
-                      <ul>
-                        {filteredData &&
-                          filteredData.map((item) => (
-                            <li
-                              key={item.id}
-                              onClick={() => handleItemClick(item.title)}
-                            >
-                              <a href={`/product/${item.id}`}>{item.title}</a>
-                            </li>
-                          ))}
-                      </ul>
-                    )}
-                  </div>
-                </div>
-                <div className={Style.search}>
-                  <i className="fa-solid fa-magnifying-glass"></i>
-                </div>
+            </div>
+            <div className={Style.iconbar}>
+              <i className="fa-brands fa-whatsapp"></i>
+            </div>
+            <div className={Style.inquery} onClick={showDiv}>
+              <a href="#/">INQUIRE NOW</a>
+              <div className={Style.emailbar}>
+                <i className="fa-solid fa-envelope"></i>
               </div>
             </div>
-          </div>
-
-          <div className={Style.maintext}>
-            <div className={Style.textbar}>
-              <h2>CREDO INVESTMENTS FZE</h2>
-              <p>Your search for the luxury home in Dubai ends here.</p>
-              <div className={Style.knowbar}>
-                <a href="/about">KNOW MORE</a>
+            <div className={Style.searchbar}>
+              <div className={Style.searchvalue}>
+                <input
+                  type="search"
+                  placeholder="Search"
+                  value={search}
+                  onChange={handleChange}
+                />
+                <div className={Style.mainserch}>
+                  {query && (
+                    <ul>
+                      {filteredData &&
+                        filteredData.map((item) => (
+                          <li
+                            key={item.id}
+                            onClick={() => handleItemClick(item.title)}
+                          >
+                            <a href={`/product/${item.id}`}>{item.title}</a>
+                          </li>
+                        ))}
+                    </ul>
+                  )}
+                </div>
+              </div>
+              <div className={Style.search}>
+                <i className="fa-solid fa-magnifying-glass"></i>
               </div>
             </div>
           </div>
         </div>
-      </StickyBox>
+
+        <div className={Style.maintext}>
+          <div className={Style.textbar}>
+            <h2>CREDO INVESTMENTS FZE</h2>
+            <p>Your search for the luxury home in Dubai ends here.</p>
+            <div className={Style.knowbar}>
+              <a href="/about">KNOW MORE</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
